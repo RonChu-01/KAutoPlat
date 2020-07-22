9cad314c6e792dcb70ae6681c4297f1bf71aa2c4
chuyong <chuyong@3k.com>
2020-07-22 15:43:04 +0800

    新增退出登录视图

--- core/blueprint/auth/logout.py
+++ core/blueprint/auth/logout.py
@@ -2,3 +2,17 @@
 # Created by #chuyong, on 2020/7/21 11:34 上午.
 # Copyright (c) 2020 3KWan.
 # Description :
+
+
+from flask import Blueprint, redirect, url_for, session
+
+
+bp = Blueprint("logout", __name__, url_prefix="/logout")
+
+
+@bp.route("/")
+def logout():
+    session.pop("username", "")
+    return redirect(url_for("login.login"))
+
+
