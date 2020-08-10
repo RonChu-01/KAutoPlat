80907ccb5c0c24481c1a2ed87464bbb1a3ba24ed
chuyong <chuyong@3k.com>
2020-08-10 15:45:11 +0800

    接口任务管理页面，新增逻辑处理

--- core/blueprint/interface/interface.py
+++ core/blueprint/interface/interface.py
@@ -3,14 +3,52 @@
 # Copyright (c) 2020 3KWan.
 # Description :
 
+import random
+
+from faker import Faker
 from flask import Blueprint, render_template, jsonify
 
 bp = Blueprint("interface", __name__, url_prefix="/interface")
 
+channel = {
+    "3k": "3k",
+    "360": "360",
+    "mi": "小米",
+    "91": "91",
+    "wandou": "豌豆荚",
+    "dangle": "当乐",
+    "oppo": "oppo",
+    "duoku": "百度",
+    "uc": "uc",
+    "huawei": "华为",
+    "vivo": "步步高",
+    "4399": "4399"
+}
+
 
 @bp.route("/")
 def index():
-    return render_template("interface/index.html")
+
+    tasks = []
+
+    fake = Faker("zh_CN")
+
+    for i in range(50):
+        task_info = dict()
+        task_info.setdefault("task_id", i)
+        task_info.setdefault("channel", random.choice([_ for _ in channel.keys()]))
+        task_info.setdefault("buss_type", random.choice([_ for _ in ["国内", "独立", "海外"]]))
+        task_info.setdefault("version", "v4")
+        task_info.setdefault("task_type", random.choice([_ for _ in ["自建", "jenkins"]]))
+        task_info.setdefault("task_status", random.choice([_ for _ in ["成功", "失败", "排队中", "执行中", "未执行"]]))
+        task_info.setdefault("notice", random.choice([_ for _ in ["发送成功", "发送失败", "未发送"]]))
+        task_info.setdefault("sponsor", random.choice([_ for _ in ["admin", "test", "dev"]]))
+        task_info.setdefault("create_time", fake.date_time(tzinfo=None))
+        task_info.setdefault("complete_time", fake.date_time(tzinfo=None))
+        task_info.setdefault("test_report", "https://wwww.baidu.com")
+        tasks.append(task_info)
+
+    return render_template("interface/index.html", tasks=tasks)
 
 
 # ======================== 数据接口 ========================
@@ -130,4 +168,9 @@ def api_v1_task_status():
         "msg": "",
         "data": data
     })
+
+
+if __name__ == '__main__':
+    data = [1, 2, 2, 3, 5, 6]
+    print(random.choice([c for c in channel.keys()]))
 
