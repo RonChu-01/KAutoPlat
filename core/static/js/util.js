0cb49ff2abf5c838fe7c2f2013467908e0edf7cd
chuyong <chuyong@3k.com>
2020-08-12 17:57:02 +0800

    添加部分插件

--- core/static/js/util.js
+++ core/static/js/util.js
@@ -6,9 +6,18 @@
 
 var host = "http://127.0.0.1:10010";  //域名
 
-//http请求方法封装（ajax）
 function http(url, data, method, success, fail) {
+    /**
+     * http请求方法封装（ajax）
+     * @param url
+     * @param data
+     * @param method
+     * @param success
+     * @param fail
+     */
+
     var data = method == 'GET' ? data : JSON.stringify(data);
+
     $.ajax({
         type: method,
         contentType: "application/json; charset=utf-8",
@@ -18,4 +27,26 @@ function http(url, data, method, success, fail) {
         success: success,  //成功回调
         fail: fail  //失败回调
     });
+
 }
+
+
+String.prototype.format = function () {
+    /**
+     * format 函数（方便动态渲染）
+     * var str = "js实现用{two}自符串替换占位符{two} {three}  {one} ".format({one: "I",two: "LOVE",three: "YOU"});
+     * var str2 = "js实现用{1}自符串替换占位符{1} {2}  {0} ".format("I","LOVE","YOU");
+     */
+    if (arguments.length == 0) return this;
+    var param = arguments[0];
+    var s = this;
+    if (typeof (param) == 'object') {
+        for (var key in param)
+            s = s.replace(new RegExp("\\{" + key + "\\}", "g"), param[key]);
+        return s;
+    } else {
+        for (var i = 0; i < arguments.length; i++)
+            s = s.replace(new RegExp("\\{" + i + "\\}", "g"), arguments[i]);
+        return s;
+    }
+};
